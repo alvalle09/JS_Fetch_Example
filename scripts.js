ccaa8c5025256ae8f2eba18d98d136a0711c0cbe
Fred Valle <phredvalle@gmail.com>
2019-01-22 10:51:26 -0600

    final changes, adds error handling

--- scripts.js
+++ scripts.js
@@ -3,23 +3,23 @@ const ul = document.getElementById('authors');
 const url = 'https://randomuser.me/api/?results=10';
 
 fetch(url)
-.then((resp) => resp.json())
-.then(function(data) {
-  let authors = data.results;
-  return authors.map(function(author) {
-    let li = createNode('li'),
-        img = createNode('img'),
-        span = createNode('span');
-    img.src = author.picture.medium;
-    span.innerHTML = `${author.name.first} ${author.name.last}`;
-    append(li, img);
-    append(li, span);
-    append(ul, li);
-  })
-})
-.catch(function(error) {
-  console.log(error);
-});   
+    .then((resp) => resp.json())
+    .then(function(data) {
+        let authors = data.results;
+        return authors.map(function(author) {
+            let li = createNode('li'),
+            img = createNode('img'),
+            span = createNode('span');
+            img.src = author.picture.medium;
+            span.innerHTML = `${author.name.first} ${author.name.last}`;
+            append(li, img);
+            append(li, span);
+            append(ul, li);
+        })
+    })
+    .catch(function(error) {
+        console.log(error);
+    });   
 
 function createNode(element) {
     return document.createElement(element);
